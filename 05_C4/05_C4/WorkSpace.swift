02a73ab72bc834248c737489e4ed589633e94934
Shin <Shin@s-enomoto.private.i-studio.co.jp>
2016-03-22 16:59:47 +0900

    Add pan gesture function

--- 05_C4/05_C4/WorkSpace.swift
+++ 05_C4/05_C4/WorkSpace.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class WorkSpace: CanvasController {
+    
+    var petris = [Circle]()
 
     override func setup() {
-        let microbe = createMicrobe(canvas.center)
-        randomMove(microbe)
-        canvas.add(microbe)
+//        let microbe = createMicrobe(canvas.center)
+//        randomMove(microbe)
+//        canvas.add(microbe)
+        createPetris()
+        
+        //ジェスチャー追加
+        canvas.addPanGestureRecognizer { (locations, center, translation, velocity, state) -> () in
+            ShapeLayer.disableActions = true
+            let microbe = self.createMicrobe(center)
+            self.canvas.add(microbe)
+            
+//            self.randomMove(microbe)
+//            self.fade(microbe)
+            let petri = self.petris[random(below: self.petris.count)]
+            self.moveToCenter(microbe, ofPetri: petri)
+            
+            ShapeLayer.disableActions = false
+        }
     }
     
     func createMicrobe(center: Point) -> Circle {
-        let microbe = Circle(center: center, radius: 50)
+        let microbe = Circle(center: center, radius: 2)
         microbe.lineWidth = 0
         microbe.fillColor = C4Pink
         
@@ -32,15 +49,77 @@ class WorkSpace: CanvasController {
     }
     
     func randomMove(microbe: Circle) {
-        let randomMove = ViewAnimation(duration: 0.5) {
-            microbe.center = Point(random01()*self.canvas.width,random01()*self.canvas.height)
+//        let randomMove = ViewAnimation(duration: 0.5) {
+//            microbe.center = Point(random01()*self.canvas.width,random01()*self.canvas.height)
+//        }
+//        randomMove.addCompletionObserver { () -> Void in
+//            randomMove.animate()
+//        }
+//        randomMove.animate()
+        
+        let anim = ViewAnimation(duration: random01() + 2.0){ () -> Void in
+            let theta = random01() * 2 * M_PI
+            let r = 50 * random01()
+            let c = Point(r * cos(theta), r * sin(theta)) + Vector(self.canvas.center)
+            microbe.center = c
         }
-        randomMove.addCompletionObserver { () -> Void in
-            randomMove.animate()
+        anim.delay = random01()
+        anim.addCompletionObserver{ () ->  Void in
+            self.randomMove(microbe)
         }
-        randomMove.animate()
+        anim.animate()
+    }
+    
+    func fade(microbe: Circle) {
+        let a = ViewAnimation(duration: 0.25) {
+            microbe.opacity = 0.0
+        }
+        a.addCompletionObserver { () -> Void in
+            microbe.removeFromSuperview()
+        }
+        a.delay = random01() * 5 + 3
+        a.animate()
     }
 
-
+    func moveToCenter(microbe: Circle, ofPetri petri: Circle){
+        let a = ViewAnimation(duration: random01() * 0.5 + 0.5){ () -> Void in
+            microbe.center = self.canvas.center
+        }
+        a.addCompletionObserver(){ () -> Void in
+            microbe.removeFromSuperview()
+            microbe.center = petri.bounds.center
+            petri.add(microbe)
+            self.randomMove(microbe)
+            self.fade(microbe)
+        }
+        a.delay = random01() * 2
+        a.animate()
+    }
+    
+    func createPetris() {
+        let r = 150.0
+        for _ in 0...5 {
+            let petri = Circle(center: canvas.center, radius: r)
+            petri.fillColor = clear
+            petri.strokeColor = clear
+            petri.interactionEnabled = false
+            randomRotate(petri)
+            petris.append(petri)
+            canvas.add(petri)
+        }
+    }
+    
+    func randomRotate(petri: Circle) {
+        let anim = ViewAnimation(duration: random01() * 5 + 2.0) { () -> Void in
+            let θ = random01() * M_PI
+            let d = round(random01()) == 0 ? -1.0 : 1.0
+            petri.transform.rotate(θ * d)
+        }
+        anim.delay = random01()
+        anim.addCompletionObserver { () -> Void in
+            self.randomRotate(petri)
+        }
+        anim.animate()
+    }
 }
 
